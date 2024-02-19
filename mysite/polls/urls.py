bd989b3947e27eddaaafb4e9d0eb446fb6128486
palib <pavlo.m.semeniuk@ukd.edu.ua>
2024-02-19 20:10:05 +0200

    Add requirements.txt, class-based view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # шлях - /polls/, в’юха - views.index, назва в’юхи - “index”
     path("", views.index, name="index"),
+    # шлях - /polls/class, в’юха - views.IndexView, назва в’юхи - “class”
+    path("class", views.IndexView.as_view(), name="class"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,16 @@
+import asyncio
+
 from django.http import HttpResponse
+from django.views import View
 
 
 def index(request):
     name = request.GET.get('name', 'world')
     return HttpResponse(f"Hello, {name}. You're at the polls")
+
+
+class IndexView(View):
+    async def get(self, request):
+        name = request.GET.get('name', 'world')
+        await asyncio.sleep(1)
+        return HttpResponse(f"Hello, {name}. You're at the class-based polls")
